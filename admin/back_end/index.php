7d5fc95cfc73e438b2609b2a83c4efebf5088f5e
janethp <janeth.perez17@gmail.com>
2016-10-07 14:53:01 -0400

    En proceso

---
+++ admin/back_end/index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.0/jquery-ui.min.js"></script>
+
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+  <title>Punto 94.7 fm</title>
+
+  <!-- CSS  -->
+
+	  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	  <link href="../../css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+	  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+	<link href="ihover.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+	 <link href="css/modal.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+	  <script src= "js/script.js" type="text/javascript"></script>
+
+	  <script src= "js/eskju.jquery.scrollflow.min.js" type="text/javascript"></script>
+
+	<link rel="stylesheet" type="text/css" href="normalize.css" />
+		
+	<link rel="stylesheet" type="text/css" href="component.css" />
+	<script src="modernizr.min.js"></script>
+
+</head>
+<body>
+
+<div class="container">
+
+  
+
+    <form class="col s12">
+      <h1 ><p class="center">Acceso</p></h1>
+        <br>
+        <br>
+     
+            <div class="container block">
+            <div class="container block">
+              <div class="input-field ">
+                <i class="material-icons prefix">account_circle</i>
+                <input id="icon_prefix" type="text" class="validate">
+                <label for="icon_prefix">Usuario</label>
+              </div>
+           
+          
+              <div class="input-field">
+                <i class="material-icons prefix">lock</i>
+                <input id="icon_telephone" type="tel" class="validate">
+                <label for="icon_telephone">Contraseña</label>
+              </div>
+              
+ <br>
+ <br>
+   <center><button class=" col s12 btn waves-effect waves-light" type="submit" name="action">Submit
+    <i class="material-icons right">send</i>
+  </button></center>
+
+
+
+              </div>
+              </div>
+            
+
+  </form>
+
+
+
+</div>
+
+
+
+  <!--  Scripts-->
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+
+  <script src="../../js/materialize.js"></script>
+
+</body>
+</html>
---
+++ js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+ 	
+	$("#in").css({"height": $(window).height() + "px"});
+
+	$("#in2").css({"height": $(window).height() + "px"});
+
+	$("#in4").css({"height": $(window).height() + "px"});
+ 
+	$("#in5").css({"height": $(window).height() + "px"});
+
+	
+
+	$("#galery").css({"height": $(window).height() + "px"});
+
+
+	var flag = false;
+	var scroll;
+
+	
+
+
+
+
+});
